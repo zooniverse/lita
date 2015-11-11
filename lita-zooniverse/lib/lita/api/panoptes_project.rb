5739b67ffc0ed09c26018af22a6f610674761386
Campbell Allen <campbell.allen@gmail.com>
2015-11-11 18:17:39 +0000

    report all projects irrespective of migration status
    show the zoo home project id for legacy migrated panoptes projects. allow the user to figure out which id they should use.

--- lita-zooniverse/lib/lita/api/panoptes_project.rb
+++ lita-zooniverse/lib/lita/api/panoptes_project.rb
@@ -6,30 +6,42 @@ module Lita
 
       include Api::Project
 
-      def projects
-        results = JSON.parse(projects_search)
-        results["projects"].map do |project|
-          next if project["migrated"]
-          "(P): #{project_url(project)} -- #{project["id"]} -- #{project["display_name"]} (classifications: #{project["classifications_count"]}, classifiers: #{project["classifiers_count"]}, live: #{project["live"]})"
-	      end.compact
+      def self.api_headers
+        {
+          "Content-Type" => "application/json",
+          "Accept" => "application/vnd.api+json; version=1"
+        }
+      end
+
+      def self.api_host
+        @api_host ||= "https://www.zooniverse.org/api/projects"
+      end
+
+      def self.projects(search_query)
+        projects = HTTParty.get(api_host, { headers: api_headers, query: { search: search_query } })
+        results = JSON.parse(projects)
+        results["projects"].map { |project| self.new(project) }.compact
+      end
+
+      def to_s
+        "(P): #{project_url(project)} -- #{project["id"]} -- #{project["display_name"]} "\
+        "(classifications: #{project["classifications_count"]}, "\
+        "classifiers: #{project["classifiers_count"]}, live: #{project["live"]}"\
+        "#{migrated? ? ", zoo_project_id: #{legacy_zoo_project_id}" : ""})"
       end
 
       private
 
-      def api_host
-        @api_host ||= "https://www.zooniverse.org/api/projects"
-      end
-
-      def projects_search
-        HTTParty.get(api_host, { headers: api_headers, query: { search: search_query } })
+      def migrated?
+        project["migrated"]
       end
 
       def project_url(project)
         "https://www.zooniverse.org/projects/#{project["slug"]}"
       end
 
-      def api_headers
-        super.merge("Accept" => "application/vnd.api+json; version=1")
+      def legacy_zoo_project_id
+        project["configuration"]["zoo_home_project_id"]
       end
     end
   end
