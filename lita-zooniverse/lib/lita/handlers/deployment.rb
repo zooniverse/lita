151772ba74ef1e7efb47570a0695717a26dba82d
Marten Veldthuis <marten@veldthuis.com>
2015-12-10 16:06:57 +0000

    Add deployment task for clearing the static cache

--- lita-zooniverse/lib/lita/handlers/deployment.rb
+++ lita-zooniverse/lib/lita/handlers/deployment.rb
@@ -19,6 +19,7 @@ module Lita
       route(/^panoptes deploy/, :deploy, command: true, help: {"panoptes deploy" => "Triggers a deployment of *PRODUCTION* in Jenkins."})
       route(/^panoptes lock\s*(.*)/, :lock, command: true, help: {"panoptes lock REASON" => "Stops builds and deployments"})
       route(/^panoptes unlock/, :unlock, command: true, help: {"panoptes unlock" => "Lifts deployment restrictions"})
+      route(/^clear static cache/, :clear_static_cache, command: true, help: {"clear static cache" => "Clears the static cache (duh)"})
 
       def status(response)
         ensure_no_lock(response)
@@ -62,6 +63,9 @@ module Lita
         end
       end
 
+      def clear_static_cache(response)
+        build_jenkins_job(response, "Clear static cache")
+      end
 
       private
 
