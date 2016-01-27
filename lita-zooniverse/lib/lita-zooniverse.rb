3c7530717512b6d30d11b8934174e22f9c607495
Marten Veldthuis <marten@veldthuis.com>
2016-01-27 14:35:01 +0000

    Add lintott locator

--- lita-zooniverse/lib/lita-zooniverse.rb
+++ lita-zooniverse/lib/lita-zooniverse.rb
@@ -6,8 +6,9 @@ Lita.load_locales Dir[File.expand_path(
 
 require "lita/handlers/projects"
 require "lita/handlers/deployment"
-require "lita/handlers/reload"
+require "lita/handlers/lintott"
 require "lita/handlers/aws_handler"
+require "lita/handlers/reload"
 
 Lita::Handlers::Projects.template_root File.expand_path(
   File.join("..", "..", "templates"),
