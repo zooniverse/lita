3709932d6a668bde4d414f7920d5247cb11f2cba
Marten Veldthuis <marten@veldthuis.com>
2017-05-31 16:14:52 +0100

    Fix dad joke

--- handlers/dad_handler.rb
+++ handlers/dad_handler.rb
@@ -1,6 +1,6 @@
 module Lita
 	class DadHandler < Handler
-		route /^dad\s?joke$/, :tell_joke, command: true
+		route /dad\s?joke/, :tell_joke, command: true
 
     def tell_joke(response)
       joke = HTTParty.get("https://icanhazdadjoke.com", headers: {"Accept" => "text/plain"}).strip
