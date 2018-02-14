379eb4c838da02be4acafe286c50f488ea1af9dc
Campbell Allen <campbell.allen@gmail.com>
2018-02-14 13:20:06 +0000

    cleanup start.sh script
    remove commands for rails apps / panoptes

--- docker/start.sh
+++ docker/start.sh
@@ -1,34 +1,10 @@
 #!/bin/bash -ex
-
-cd /rails_app
-
-if [ -d "/rails_conf/" ]
-then
-    ln -sf /rails_conf/* ./config/
-fi
-
 mkdir -p tmp/pids/
 rm -f tmp/pids/*.pid
 
-if [ "$RAILS_ENV" == "development" ]; then
-  exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
-else
-  USER_DATA=$(curl --fail http://169.254.169.254/latest/user-data || echo "")
-
-  if [ "$USER_DATA" == "EMERGENCY_MODE" ]
-  then
-    git pull
-  fi
-
-  if [ -f "commit_id.txt" ]
-  then
-    cp commit_id.txt public/
-  fi
-
-  if [ -f /run/secrets/environment ]
-  then
-    source /run/secrets/environment
-  fi
-
-  exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
+if [ -f /run/secrets/environment ]
+then
+  source /run/secrets/environment
 fi
+
+exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
