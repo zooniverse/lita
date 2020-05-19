4f21bf5b2222cbb918f7b9a55f5821e69d21faa8
Campbell Allen <campbell.allen@gmail.com>
2020-05-19 16:17:06 +0100

    spec loading the lita handlers

--- lita_config.rb
+++ lita_config.rb
@@ -1,15 +1,7 @@
 require 'bundler'
 require_relative './lita_env'
 
-require_relative 'handlers/aws_handler'
-require_relative 'handlers/beverage_handler'
-require_relative 'handlers/dad_handler'
-require_relative 'handlers/deployment'
-require_relative 'handlers/insult_handler'
-require_relative 'handlers/lintott'
-require_relative 'handlers/projects'
-require_relative 'handlers/tell_handler'
-require_relative 'handlers/project_name_handler'
+require_relative './load_handlers'
 
 Bundler.require(:default, Lita::env)
 Dotenv.load(ENV["DOTENV_FILE"] || '.env')
---
+++ load_handlers.rb
@@ -0,0 +1,8 @@
+require_relative 'handlers/aws_handler'
+require_relative 'handlers/dad_handler'
+require_relative 'handlers/deployment'
+require_relative 'handlers/insult_handler'
+require_relative 'handlers/lintott'
+require_relative 'handlers/projects'
+require_relative 'handlers/tell_handler'
+require_relative 'handlers/project_name_handler'
