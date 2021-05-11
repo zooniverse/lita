b545fc36adb6db22f12dc0913b772bdbe4680458
Jim O'Donnell <jim@zooniverse.org>
2021-05-11 16:07:27 +0100

    Add a status URL for translations Alias `zooniverse/pandora` to translations.zooniverse.org.

--- lib/github_status_reporter.rb
+++ lib/github_status_reporter.rb
@@ -13,6 +13,7 @@ module Lita
       class UnknonwnRepoCommit < StandardError; end
 
       IRREGULAR_ORG_URLS = {
+        'zooniverse/pandora' => 'https://translations.zooniverse.org/commit_id.txt',
         'zooniverse/talk-api' => 'https://talk.zooniverse.org/commit_id.txt',
         'zooniverse/zoo-stats-api-graphql' => 'https://graphql-stats.zooniverse.org',
         'zooniverse/zoo-event-stats' => 'https://stats.zooniverse.org/'
