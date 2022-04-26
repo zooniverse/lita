18c0780c844e72195f8a0ae18a8b070a324b450f
Campbell Allen <campbell.allen@gmail.com>
2022-04-26 11:49:01 +0100

    case insensitive repo url name matching

--- lib/zooniverse_github.rb
+++ lib/zooniverse_github.rb
@@ -17,10 +17,11 @@ module Lita
 
       class RefAlreadyDeployed < StandardError; end
 
-      IRREGULAR_ORG_URLS = {
+      # ensure these are all downcased for easy matching
+      IRREGULAR_DOWNCASED_ORG_URLS = {
         'zooniverse/front-end-monorepo' => 'https://fe-project.zooniverse.org/projects/commit_id.txt',
         'zooniverse/pfe-lab' => 'https://lab.zooniverse.org/commit_id.txt',
-        'zooniverse/Panoptes-Front-End' => 'https://www.zooniverse.org/commit_id.txt',
+        'zooniverse/panoptes-front-end' => 'https://www.zooniverse.org/commit_id.txt',
         'zooniverse/pandora' => 'https://translations.zooniverse.org/commit_id.txt',
         'zooniverse/talk-api' => 'https://talk.zooniverse.org/commit_id.txt',
         'zooniverse/zoo-stats-api-graphql' => 'https://graphql-stats.zooniverse.org',
@@ -240,7 +241,7 @@ module Lita
       end
 
       def repo_type_and_url(repo_name)
-        url = IRREGULAR_ORG_URLS[repo_name]
+        url = IRREGULAR_DOWNCASED_ORG_URLS[repo_name.downcase]
         if url
           url
         else
