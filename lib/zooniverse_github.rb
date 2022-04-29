bc2dcc36858f979189e7bab335693efe58b63b8c
Jim O'Donnell <jim@zooniverse.org>
2022-04-29 08:45:44 +0100

    Add Scribes Of The Cairo Geniza to GitHub statuses

--- lib/zooniverse_github.rb
+++ lib/zooniverse_github.rb
@@ -23,6 +23,7 @@ module Lita
         'zooniverse/pfe-lab' => 'https://lab.zooniverse.org/commit_id.txt',
         'zooniverse/panoptes-front-end' => 'https://www.zooniverse.org/commit_id.txt',
         'zooniverse/pandora' => 'https://translations.zooniverse.org/commit_id.txt',
+        'zooniverse/scribes-of-the-cairo-geniza' => 'https://www.scribesofthecairogeniza.org/commit_id.txt',
         'zooniverse/talk-api' => 'https://talk.zooniverse.org/commit_id.txt',
         'zooniverse/zoo-stats-api-graphql' => 'https://graphql-stats.zooniverse.org',
         'zooniverse/zoo-event-stats' => 'https://stats.zooniverse.org/'
