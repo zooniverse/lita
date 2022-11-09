409799ed88aa59c742b59b577e5469abf623f86f
Campbell Allen <campbell.allen@gmail.com>
2022-11-09 05:22:44 +0000

    add bajor to list of branches that use main (#161)
    * add bajor to list of branches that use main
    
    * add comma

--- lib/zooniverse_github.rb
+++ lib/zooniverse_github.rb
@@ -37,7 +37,8 @@ module Lita
       # Repos that do not use heads/master as their primary ref
       PRIMARY_REF_BY_REPO = {
         'zooniverse/pandora' => 'heads/main',
-        'zooniverse/kade' => 'heads/main'
+        'zooniverse/kade' => 'heads/main',
+        'zooniverse/bajor' => 'heads/main'
       }.freeze
 
       JSON_COMMIT_ID_KEYS = %w[revision commit_id].freeze
